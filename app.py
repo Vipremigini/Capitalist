dd3ed19f44413125d24a73ae4b58ae01c2bd4956
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-02 13:18:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request
 import random
+import psycopg2
+import os
+
+connection = psycopg2.connect(database="verceldb", user='default', password=os.environ['POSTGRES_PASSWORD'], host=os.environ["POSTGRES_HOST"])
+
+cur = connection.cursor()
 
 f = open("facts.txt","r")
 lines = f.readlines()
@@ -11,7 +17,7 @@ def trial():
     leng = len(lines)
     randnum = random.randint(0, leng)
     fact = lines[randnum]
-    
+    cur.execute("insert into try values("adi")")
     
     return {"blocks": [
     {
