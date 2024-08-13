7b17ff283aea63def5aab6c59562075b87067c45
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-13 09:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 from flask import Flask, request
-from slack_sdk import WebClient
 from slack_bolt import App, Say
 from slack_bolt.adapter.flask import SlackRequestHandler
 app = Flask(__name__)
-client = WebClient(token=os.environ.get("buoat")
 bolt_app = App(token=os.environ.get("buoat"), signing_secret=os.environ.get("sss"))
 handler = SlackRequestHandler(bolt_app)
 
