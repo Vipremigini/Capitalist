e7651c3c8391d9368d8d3d42edb05680f27883f2
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-13 10:09:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import Flask, request
 from slack_bolt import App, Say
 from slack_bolt.adapter.flask import SlackRequestHandler
 app = Flask(__name__)
-bolt_app = App(token=os.environ.get('buoat'), signing_secret=os.environ.get('sss')))
+bolt_app = App(token=os.environ.get('buoat'), signing_secret=os.environ.get('sss'))
 handler = SlackRequestHandler(bolt_app)
 
 
 @app.route("/api/events", methods=["POST"])
 def slack_events():
-    """ Declaring the route where slack will post a request """
+ 
     return handler.handle(request)
 
 @bolt_app.message("hello slacky")
 def greetings(payload: dict, say: Say):
-    """ This will check all the message and pass only those which has 'hello slacky' in it """
+   
     user = payload.get("user")
     say(f"Hi <@{user}>")
 
