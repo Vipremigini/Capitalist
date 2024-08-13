7afeba3b34c95cd881e98a896852432e5ddd16b0
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-13 09:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import os
-from slack_sdk import WebClient as app
-from slack_sdk.errors import SlackApiError 
+from flask import Flask, request
+from slack_sdk import WebClient
+from slack_bolt import App, Say
+from slack_bolt.adapter.flask import SlackRequestHandler
+app = Flask(__name__)
+client = WebClient(token=os.environ.get("buoat")
+bolt_app = App(token=os.environ.get("buoat"), signing_secret=os.environ.get("sss"))
+handler = SlackRequestHandler(bolt_app)
 
-slack_token = str(os.environ.get('buoat'))
+
+@app.route("/api/events", methods=["POST"])
+def slack_events():
+    """ Declaring the route where slack will post a request """
+    return handler.handle(request)
+
+@bolt_app.message("hello slacky")
+def greetings(payload: dict, say: Say):
+    """ This will check all the message and pass only those which has 'hello slacky' in it """
+    user = payload.get("user")
+    say(f"Hi <@{user}>")
 
 
-client = app(token=slack_token)
-
-try:
-	response = client.chat_postMessage(
-    				channel="C07F7MP99MH",
-    				text="Bot's first message")
-	
-	
-except SlackApiError as e:
-	assert e.response["error"]
-	
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
