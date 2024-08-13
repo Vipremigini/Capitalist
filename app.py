fd75b022734d55a80ff33808eaaf0dbc5b4617a9
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-13 09:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,18 @@
-from flask import Flask, request
-import random
-import psycopg2
 import os
-import requests
+from slack_sdk import WebClient 
+from slack_sdk.errors import SlackApiError 
 
-con = psycopg2.connect(database="verceldb", user='default', password=os.environ['POSTGRES_PASSWORD'], host=os.environ["POSTGRES_HOST"])
-
-cur = con.cursor()
-
-
-app = Flask(__name__)
-
-@app.post("/api/register")
-def register():
-    uid = request.form.get("user_id")
-    comm = "select uid from userdata where uid = '" + uid + "'"
-    cur.execute(str(comm))
-    data = cur.fetchall()
-    if data == []:
-        comm = "insert into userdata values('" + uid + "' , 500);"
-        cur.execute(comm)
-        con.commit()
-        return { "response_type": "in_channel",
-                "blocks": [
-            {
-                "type": "section",
-                "response_type": "in_channel",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Registered successfully!! You now have 500 hack dollars"
-                }
-            }]}
-    else:
-        return { "response_type": "in_channel",
-                "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "You are already registered"
-                }
-            }]}
-        
-@app.post("/api/interact")
-def reply():
-    purl = "https://slack.com/api/chat.postMessage"
-    data = request.form
-    data = str(data)
-    
-    obj = {"channel": "C07F7MP99MH","text": data}
-    x = requests.post(url,Authorization = os.environ['buoat'],  json = obj)
+slack_token = os.environ.get('buoat')
 
 
-@app.post("/api/buy")
-def buy():
-    return { "response_type": "in_channel",
-	"blocks": [
-		{
-			"type": "input",
-			"element": {
-				"type": "plain_text_input",
-				"action_id": "plain_text_input-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "hi",
-				
-			}
-		}
-	]
-}
+client = WebClient(token=slack_token)
 
-
-
+try:
+	response = client.chat_postMessage(
+    				channel="C07F7MP99MH",
+    				text="Bot's first message")
+	
+	
+except SlackApiError as e:
+	assert e.response["error"]
+	
